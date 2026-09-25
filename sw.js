322069ed68a155018f52f0e4b8ae5564b594c653
tmpalala-crypto <tmpalala@gmail.com>
2026-09-25 08:24:03 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('qiniso-v25').then(cache => {
+      return cache.addAll([
+        '/Qiniso-dashboard-/scan.html',
+        '/Qiniso-dashboard-/security-lock-v25.js',
+        '/Qiniso-dashboard-/logo.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
