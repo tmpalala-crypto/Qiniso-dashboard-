c47e7e6b3a67d26efcde5ebc659cd8466d20d302
tmpalala-crypto <tmpalala@gmail.com>
2026-09-25 09:13:50 +0200

    Update logo file path in service worker

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', e => {
       return cache.addAll([
         '/Qiniso-dashboard-/scan.html',
         '/Qiniso-dashboard-/security-lock-v25.js',
-        '/Qiniso-dashboard-/logo.png'
+        '/Qiniso-dashboard-/logo.png2'
       ]);
     })
   );
